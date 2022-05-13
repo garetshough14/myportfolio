f541873198cfc57a0d4857788dc274938db11f56
Garet Hough <garetshough@gmail.com>
2022-05-13 11:26:10 -0700

    rework(particles): removeed all particle files and changed gh-pages path

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import { HashRouter } from "react-router-dom";
 import Content from "./Components/Content";
-import ParticleBackground from "./Components/ParticleBackground";
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
         className=" container-sm h-100 d-flex flex-column"
         style={{ maxWidth: "800px" }}
       >
-        <ParticleBackground />
         <HashRouter>
           <Navbar />
           <Content />
